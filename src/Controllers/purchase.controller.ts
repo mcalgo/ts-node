632ad1279f77a8c68edeaafd1b7bc27188c1aa88
Juan Sebastian Arevalo Gomez <jsarevalo.gomez@gmail.com>
2021-09-10 18:44:51 -0500

    se ajusta el controlador de purchase

--- src/Controllers/purchase.controller.ts
+++ src/Controllers/purchase.controller.ts
@@ -1,25 +1,64 @@
+import { Service } from "typedi";
+import { Request ,Response } from 'express';
+import { PurchaseService } from "../Services/purchase.service";
+import { CreatePurchase } from "../Models/Purchase/createPurchase.model";
+import { UpdatePurchase } from "../Models/Purchase/updatePurchase.model";
+
+@Service()
 export class PurchaseController{
     constructor(
-
+        private readonly purchaseService : PurchaseService
     ){}
 
-    getPurchases(){
+    getPurchases(req: Request, res: Response){
+        const purchase = this.purchaseService.findAllPurchase();
+        return res.json({
+            purchase: purchase
+        });
+    }
+
+    getPurchase(req: Request, res: Response){
+
+        const { purchaseID } = req.body;
+        const user = this.purchaseService.findOnePurchase(purchaseID);
+        
+        return res.json({
+            user: user
+        });
 
     }
 
-    getPurchase(){
+    postPurchase(req: Request, res: Response){
 
+        const newPurchase: CreatePurchase = req.body;
+        const createProduct = this.purchaseService.CreatePurchase(newPurchase);
+    
+        return res.json({
+            ok: true,
+            new_user: createProduct
+        }); 
     }
 
-    postPurchase(){
+    putPurchase(req: Request, res: Response){
 
-    }
-
-    putPurchase(){
-
+        const { purchaseID } = req.body;
+        const purchase: UpdatePurchase = req.body;
+    
+        const updateUser = this.purchaseService.updatePurchase(purchaseID, purchase);
+    
+        return res.json({
+            updateUser
+        });
     } 
     
-    delectPurchase(){
+    delectPurchase(req: Request, res: Response){
 
+        const { purchaseID } = req.body;
+    
+        const user = this.purchaseService.deletPurchase(purchaseID);
+
+        return res.json({
+            user
+        });
     }
 }
