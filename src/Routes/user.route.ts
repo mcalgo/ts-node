55023a96e55cf08eccfa2da2ce9c6bd6b7c882fb
Juan Sebastian Arevalo Gomez <jsarevalo.gomez@gmail.com>
2021-09-10 20:51:55 -0500

    se ajusta el proyecto para utilizar cache

--- src/Routes/user.route.ts
+++ src/Routes/user.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { Container } from "typeorm-typedi-extensions";
 import { UserController } from "../Controllers/user.controller";
+import Cache from 'express-redis-cache'
 
 /*
     Route: /api/user
@@ -8,11 +9,12 @@ import { UserController } from "../Controllers/user.controller";
 
 const router = Router();
 const userController: UserController = Container.get(UserController)
+const cache = Cache();
 
 router.route('/')
-                .get( userController.getUsers.bind(userController) )
+                .get( userController.getUsers.bind(userController), cache.route() )
                  .post( userController.postUser.bind(userController) );
-router.route('/:UserId').get( userController.getUser.bind(userController) )
+router.route('/:UserId').get( userController.getUser.bind(userController), cache.route() )
                         .put( userController.putUser.bind(userController) )
                         .delete( userController.deletUser.bind(userController) );
 
